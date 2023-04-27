3d4d70955b1a569a23a12d927cb585ffc22d1c5f
Zul Ham Fahlevi <92670681+ZulHamFahlevi@users.noreply.github.com>
2023-04-27 22:05:35 +0700

    feat(dashboard): add config theme antd

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
+import { ThemeConfig } from "./theme/ThemeConfig.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ConfigProvider theme={ThemeConfig}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ConfigProvider>
 );

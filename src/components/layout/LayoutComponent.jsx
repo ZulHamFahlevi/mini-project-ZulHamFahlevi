5aa85de9031106b74b7f4bba29dd15a0553df708
Zul Ham Fahlevi <92670681+ZulHamFahlevi@users.noreply.github.com>
2023-04-27 20:29:46 +0700

    feat(routing-page): memisah codingan layout mendajadi beberapa component

--- src/components/layout/LayoutComponent.jsx
+++ src/components/layout/LayoutComponent.jsx
@@ -1,105 +1,34 @@
-import {
-  DashboardOutlined,
-  FontColorsOutlined,
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  AppstoreAddOutlined,
-} from "@ant-design/icons";
-import { Button, Layout, Menu, theme } from "antd";
-import { useState } from "react";
-import { AlfatihIcon } from "../../assets/index";
-import { Link } from "react-router-dom";
-const { Header, Sider, Content, Footer } = Layout;
+import { ArrowUpOutlined } from "@ant-design/icons";
+import { Button, Layout } from "antd";
+import FooterComponent from "./footer/FooterComponent";
+import HeaderComponent from "./header/HeaderComponent";
+import "./layout.css";
+import SidebarComponent from "./sidebar/SidebarComponent";
 
 const LayoutComponent = ({ children }) => {
-  const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+  const { Content } = Layout;
+
   return (
     <>
-      <Layout
-        style={{
-          minHeight: "100vh",
-        }}
-      >
-        <Sider
-          trigger={null}
-          collapsible
-          collapsed={collapsed}
-          style={{
-            backgroundColor: "#fff",
-          }}
-        >
-          <div className="logo" />
-          <Menu
-            theme="light"
-            mode="inline"
-            defaultSelectedKeys={[window.location.pathname]}
-            items={[
-              {
-                key: "",
-                label: (
-                  <Link to="/">
-                    <div
-                      style={{
-                        height: 30,
-                      }}
-                    >
-                      <img src={AlfatihIcon} alt="icon" width={75} />
-                    </div>
-                  </Link>
-                ),
-                icon: <FontColorsOutlined />,
-              },
-              {
-                key: "/",
-                icon: <DashboardOutlined />,
-                label: <Link to="/">Dashboard</Link>,
-              },
-              {
-                key: "/input-product",
-                icon: <AppstoreAddOutlined />,
-                label: <Link to="/input-product">Input Product</Link>,
-              },
-            ]}
-          />
-        </Sider>
+      <Layout className="site-layout">
+        <SidebarComponent />
         <Layout>
-          <Header
-            style={{
-              padding: 0,
-              background: colorBgContainer,
+          <HeaderComponent />
+          <Content className="site-layout-background">{children}</Content>
+          <FooterComponent />
+          <Button
+            className="btn-scroll-top"
+            type="primary"
+            shape="circle"
+            icon={<ArrowUpOutlined />}
+            size="large"
+            onClick={() => {
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
             }}
-          >
-            <Button
-              type="text"
-              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-              onClick={() => setCollapsed(!collapsed)}
-              style={{
-                fontSize: "16px",
-                width: 64,
-                height: 64,
-              }}
-            />
-          </Header>
-          <Content
-            style={{
-              margin: "24px 16px",
-              padding: 24,
-              minHeight: 280,
-              background: colorBgContainer,
-            }}
-          >
-            {children}
-          </Content>
-          <Footer
-            style={{
-              textAlign: "center",
-            }}
-          >
-            Ant Design ©2023 Created by Ant UED
-          </Footer>
+          />
         </Layout>
       </Layout>
     </>
--- src/components/layout/sidebar/constants.jsx
+++ src/components/layout/sidebar/constants.jsx
@@ -1,17 +1,35 @@
+import {
+  AppstoreAddOutlined,
+  DashboardOutlined,
+  FontColorsOutlined,
+} from "@ant-design/icons";
+import { AlfatihIcon } from "../../../assets/index";
 import { Link } from "react-router-dom";
 
-function getItem(label, key, icon) {
-  return {
-    key,
-    icon,
-    label,
-  };
-}
 export const MENU_ITEM = [
-  getItem(<Link to="/">Dashboard</Link>, "/"),
-  getItem(
-    <Link to="/input-product">Input Product</Link>,
-    "/input-product",
-    "icon"
-  ),
+  {
+    key: "",
+    label: (
+      <Link to="/">
+        <div
+          style={{
+            height: 30,
+          }}
+        >
+          <img src={AlfatihIcon} alt="icon" width={75} />
+        </div>
+      </Link>
+    ),
+    icon: <FontColorsOutlined />,
+  },
+  {
+    key: "/",
+    icon: <DashboardOutlined />,
+    label: <Link to="/">Dashboard</Link>,
+  },
+  {
+    key: "/input-product",
+    icon: <AppstoreAddOutlined />,
+    label: <Link to="/input-product">Input Product</Link>,
+  },
 ];

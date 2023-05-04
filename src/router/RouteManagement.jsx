da5df9ef7024ad4f4a40b09f9fbbe2e356e2338f
Zul Ham Fahlevi <zulham.fahlevi.73@gmail.com>
2023-05-04 20:03:18 +0700

    feat(login): membuat halaman login register serta membuat validasinya

--- src/router/RouteManagement.jsx
+++ src/router/RouteManagement.jsx
@@ -1,28 +1,54 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import DashboardComponent from "../page/dashboard/DashboardComponent";
 import InputProductComponent from "./../page/inputProduct/InputProductComponent";
 import LayoutUser from "../components/layoutUser/LayoutUser";
 import HomePage from "../page/homePage/HomePage";
 import ProductPage from "../page/productPage/ProductPage";
 import LayoutAdmin from "./../components/layoutAdmin/LayoutAdmin";
+import LoginPage from "./../page/loginPage/LoginPage";
+import { Suspense, useEffect } from "react";
+import LoadingComponent from "../components/loadingComponent/LoadingComponent";
 
 const RouteManagement = () => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/");
+    }
+  }, [token, navigate]);
+
   return (
     <>
-      {/* <LayoutUser>
+      <Suspense fallback={<LoadingComponent />}>
+        {!token ? (
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+          </Routes>
+        ) : (
+          <LayoutAdmin>
+            <Routes>
+              <Route path="/dashboard" element={<DashboardComponent />} />
+              <Route
+                path="/input-product"
+                element={<InputProductComponent />}
+              />
+            </Routes>
+          </LayoutAdmin>
+        )}
+      </Suspense>
+    </>
+  );
+};
+
+{
+  /* <LayoutUser>
         <Routes>
           <Route path="/home-page" element={<HomePage />} />
           <Route path="/product" element={<ProductPage />} />
         </Routes>
-      </LayoutUser> */}
-      <LayoutAdmin>
-        <Routes>
-          <Route path="/" element={<DashboardComponent />} />
-          <Route path="/input-product" element={<InputProductComponent />} />
-        </Routes>
-      </LayoutAdmin>
-    </>
-  );
-};
+      </LayoutUser> */
+}
 
 export default RouteManagement;

c358e669bf3db745b9ce867c870547a814fb4719
Zul Ham Fahlevi <zulham.fahlevi.73@gmail.com>
2023-05-05 15:26:08 +0700

    feat(user): menampilkan data product pada halaman product page menggunakan graph ql

--- src/page/productPage/ProductPage.jsx
+++ src/page/productPage/ProductPage.jsx
@@ -1,36 +1,44 @@
+import { useQuery } from "@apollo/client";
+import { List } from "antd";
 import React from "react";
+import LoadingComponent from "./../../components/loadingComponent/LoadingComponent";
 import ProductCard from "./product/ProductCard";
-import { Row, Col } from "antd";
+import { GET_PRODUCT } from "./query/form-query";
 
 const ProductPage = () => {
+  const {
+    data: dataProduct,
+    loading: loadingProduct,
+    error: errorProduct,
+  } = useQuery(GET_PRODUCT);
   return (
     <>
-      <Row>
-        <Col span={6}>
-          <ProductCard />
-        </Col>
-        <Col span={6}>
-          <ProductCard />
-        </Col>
-        <Col span={6}>
-          <ProductCard />
-        </Col>
-        <Col span={6}>
-          <ProductCard />
-        </Col>
-        <Col span={6}>
-          <ProductCard />
-        </Col>
-        <Col span={6}>
-          <ProductCard />
-        </Col>
-        <Col span={6}>
-          <ProductCard />
-        </Col>
-        <Col span={6}>
-          <ProductCard />
-        </Col>
-      </Row>
+      {loadingProduct && <LoadingComponent />}
+      <List
+        grid={{
+          gutter: 16,
+          column: 4,
+        }}
+        dataSource={dataProduct?.product}
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 8,
+        }}
+        renderItem={(item) => (
+          <>
+            <ProductCard
+              imageProduct={item.imageProduct}
+              productName={item.productName}
+              productPrice={item.productPrice}
+              productType={item.productType}
+              timeStamp={item.timeStamp}
+              uuid={item.uuid}
+            />
+          </>
+        )}
+      />
     </>
   );
 };

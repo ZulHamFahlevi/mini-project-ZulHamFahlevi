2409cb5a0c91797f535bd7bd6be4632278d40831
Zul Ham Fahlevi <zulham.fahlevi.73@gmail.com>
2023-05-08 18:51:55 +0700

    feat(detail-product): remove local storage ketika web ditutup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import RouteManagement from "./router/RouteManagement";
 
 function App() {
+  //mengahapus semua data dari local storage ketika browser di close
+  useEffect(() => {
+    const cleanup = () => {
+      localStorage.clear(); // Menghapus semua data dari local storage
+    };
+    // Register cleanup function
+    window.addEventListener("beforeunload", cleanup);
+    // Unregister cleanup function
+    return () => {
+      window.removeEventListener("beforeunload", cleanup);
+    };
+  }, []);
   return (
     <>
       <RouteManagement />

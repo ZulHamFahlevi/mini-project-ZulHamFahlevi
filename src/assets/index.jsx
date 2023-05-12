9e5e6114e88402ed4f3b3dcb49838900aff8576e
Zul Ham Fahlevi <zulham.fahlevi.73@gmail.com>
2023-05-12 20:18:03 +0700

    update_revisi: merubah gambar pada bagian home page

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -2,7 +2,7 @@ import AlfatihIcon from "./image/Alfatih-1.png";
 import AlfatihIcon2 from "./image/Alfatih-2.png";
 import AlfatihCarousel from "./image/Alfatih-carousel.jpg";
 import AlfatihCarousel2 from "./image/Alfatih-carousel-2.jpg";
-import HeroImage from "./image/keluarga.png";
+import HeroImage from "./image/hero-section.jpg";
 import LoginImage from "./image/login-image.png";
 import LogoWeb from "./image/logo-web.png";
 
